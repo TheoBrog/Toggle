9940146a9b8a082d8d1c0a1afe88fbecc667d179
Théo <theobrognoli@gmail.com>
2025-09-11 21:21:59 -0300

    trigger script and fixed screen fade names

---
+++ Assets/Scripts/General/TriggerScript.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerScript : MonoBehaviour
+{
+    public UnityEvent[] onEnterEvents;
+    public UnityEvent[] onExitEvents;
+
+    public bool singleEnter;
+    public bool singleExit;
+
+    bool hasEntered;
+    bool hasExited;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("Player") && !hasEntered)
+        {
+            foreach (UnityEvent e in onEnterEvents)
+            {
+                e.Invoke();
+            }
+
+            if (singleEnter)
+                hasEntered = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("Player") && hasExited)
+        {
+            foreach (UnityEvent e in onExitEvents)
+            {
+                e.Invoke();
+            }
+
+            if (singleExit)
+                hasExited = true;
+        }
+    }
+
+    void Reset()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+}
